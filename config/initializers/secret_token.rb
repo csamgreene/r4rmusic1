070607cc63ff1a6dbb8e4e09dcfd5814c3057152
Chris Greene <csamgreene@gmail.com>
2011-08-30 14:36:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R4rmusic1::Application.config.secret_token = '5b69db92c0ecd1dba1e13cfbbdc5f58525ae07a349e2eda858a99d5f43f50de399145c2878ed4e93715f694d49f1ee13b60443fcccfa5a2b934bb37bdd56ca13'
